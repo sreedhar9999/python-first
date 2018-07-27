2cab5d937a10f8a0b64de40866a1fbef9a477484
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-07-27 03:49:53 -0700

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 list=['a','e','i','o','u','A','E','I','O','U']
 ksc=raw_input()
-if((ksc>='a' and ind<='z') or (ksc>='A' and ksc<='Z')):
+if((ksc>='a' and ksc<='z') or (ksc>='A' and ksc<='Z')):
 	if(ksc in list):
 		print("Vowel")
 	else:
