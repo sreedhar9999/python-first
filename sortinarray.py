a204cad23a52b5eec9749078401c25fb4e0487ea
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-09 15:35:11 +0530

    Update sortinarray.py

--- sortinarray.py
+++ sortinarray.py
@@ -1,6 +1,6 @@
 # your code goes here
 N=int(input())
 if N<=100000:
-	S=[int(x) for x in raw_input().split()]
-	S.sort()
-	print" ".join(map(str,S))
+	y=[int(x) for x in raw_input().split()]
+	y.sort()
+	print" ".join(map(str,y))
