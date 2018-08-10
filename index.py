68de27edf1ece47750cef7c864aaf2206543e94c
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-10 14:28:35 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-n1=int(input())
-n2=[int(a) for a in input().split()]
-for b in range(n1):
-    print(n2[b],b,sep=' ',end="\n")
+no1=int(input())
+no2=[int(a) for a in input().split()]
+for b in range(no1):
+    print(no2[b],b,sep=' ',end="\n")
 
