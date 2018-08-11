55a2a8f3f7c01e3ed22bf63000c5bbb71857dbc9
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-11 14:43:28 +0530

    Update lenchar.py

--- lenchar.py
+++ lenchar.py
@@ -1,6 +1,6 @@
 def main():
     full_name = input("Please enter in a full name: ").split()
-    total = 0
+    t = 0
     for x in full_name:
-        total += len(x)
-    print(total)
+        t += len(x)
+    print(t)
