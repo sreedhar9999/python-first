72da1aae77b506c436f0a2f2d2efd6adcb9057d7
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-13 11:39:14 +0530

    Update hunter1.py

--- hunter1.py
+++ hunter1.py
@@ -1,9 +1,9 @@
 a=raw_input()
+c=len(a)
 q=0
 w=0
 new=[]
 list=[int(x) for x in raw_input().split()]
-z=len(list)
 while(q==0):
     l=list[q]
     list.remove(list[0])
@@ -11,7 +11,9 @@ while(q==0):
         print l,
         new.insert(w,l)
         w+=1
-    elif((list==new)and(l not in new)and(z==len(new))):
+    elif((list==new)and(l not in new)):
         print('unique')
+    else:
+        q=0
     if(list==[]):
         break
