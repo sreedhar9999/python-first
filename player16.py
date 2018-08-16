8bc29e50e94b5cc6b713fa3f1fc6045338b7a9aa
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-16 14:09:25 +0530

    Create player16.py

---
+++ player16.py
@@ -0,0 +1,17 @@
+giv=input()
+q=0
+w=0
+new=[]
+list=[int(x) for x in input().split()]
+while(q==0):
+    l=list[q]
+    list.remove(list[0])
+    if((l in list)and(l not in new)):
+        new.insert(w,l)
+        w+=1
+    elif(l not in new):
+        print(l)
+    else:
+        q=0
+    if(list==[]):
+        break
