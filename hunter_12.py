7c39f68f52493784f4703fbb3947e7aeb258796a
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-16 20:22:50 +0530

    Update and rename hunter12.py to hunter_12.py

--- hunter_12.py
+++ hunter_12.py
@@ -1,14 +1,14 @@
-n=int(input())
+num=int(input())
 list=[int(x) for x in input().split()]
-for i in range(0,n):
+for i in range(0,num):
         sum1=0
         sum2=0
         for x in range(0,i):
                 sum1=sum1+list[x]
-        for y in range(i+1,n):
+        for y in range(i+1,num):
                 sum2=sum2+list[y]
         if(sum1==sum2):
                 print('yes')
                 break
-        elif(i==(n-1)):
+        elif(i==(num-1)):
                 print('no')
