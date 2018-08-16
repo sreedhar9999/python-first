23c05a8df455e5d82752f93814d9ae13bf50a56f
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-16 14:07:34 +0530

    Update and rename player38.py to player_38.py

--- player38.py
+++
@@ -1,5 +0,0 @@
-a=int(raw_input())
-for i in range(1,a+1):
-	if((i%2)==0):
-		if((a%i)==0):
-			print i,
---
+++ player_38.py
@@ -0,0 +1,5 @@
+g=int(raw_input())
+for i in range(1,g+1):
+	if((i%2)==0):
+		if((g%i)==0):
+			print i,
