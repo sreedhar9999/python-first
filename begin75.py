8dc291efc6c36e5199e1d1ec970da12c0b814a5f
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-08-27 09:57:52 +0530

    Update begin75.py

--- begin75.py
+++ begin75.py
@@ -5,7 +5,7 @@ if((len(l)%2)!=0):
     for i in range(0,len(l)):
         print(l[i],end='')
 else:
-    l[(len(l)%2)]='*'
+    l[(len(l)//2)]='*'
     l[(len(l)//2)-1]='*'
     for i in range(0,len(l)):
         print(l[i],end='')
